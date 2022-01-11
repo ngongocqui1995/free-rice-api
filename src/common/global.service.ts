083e3d8fc85fb18af49bd20da412280b43747928
Ngô Ngọc Quí <tranphanchau10@gmail.com>
2022-01-11 12:48:22 +0700

    Update global.service.ts

--- src/common/global.service.ts
+++ src/common/global.service.ts
@@ -8,21 +8,6 @@ const global = require('global');
 export class GlobalService {
   constructor() {
     global.runJobVocabulary = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary1 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary2 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary3 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary4 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary5 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary6 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary7 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary8 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary9 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary10 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary11 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary12 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary13 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary14 = { status: false, updatedAt: moment().valueOf() };
-    global.runJobVocabulary15 = { status: false, updatedAt: moment().valueOf() };
     global.system = {
       freeMem: 0, 
       totalMem: 0, 
@@ -32,12 +17,14 @@ export class GlobalService {
     global.job = {
       index: '-1',
       username: '',
-      password: ''
+      password: '',
+      server: '',
     };
   }
 
   setJob = (value: string) => {
     let username = '';
+    let server = +value <= 15 ? 'https://free-rice-database.vercel.app' : 'https://free-rice-database-1.vercel.app';
     if (value == '0') username = 'tranphanchau10@gmail.com';
     if (value == '1') username = 'tranphanchau12@gmail.com';
     if (value == '2') username = 'tranphanchau13@gmail.com';
@@ -70,131 +57,11 @@ export class GlobalService {
     if (value == '29') username = 'pdriveanime13@gmail.com';
     if (value == '30') username = 'pdriveanime14@gmail.com';
     if (value == '31') username = 'pdriveanime15@gmail.com';
-    return global.job = { index: value, username, password: (value && 'kissmenow') };
+    return global.job = { index: value, username, password: (value && 'kissmenow'), server };
   }
 
   getJob = (): { index: string; username: string; password: string; } => {
     return global.job;
-  }
-
-  getRunJobVocabulary15 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary15;
-  } 
-
-  setRunJobVocabulary15 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary15 = {...global.runJobVocabulary15, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary14 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary14;
-  } 
-
-  setRunJobVocabulary14 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary14 = {...global.runJobVocabulary14, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary13 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary13;
-  } 
-
-  setRunJobVocabulary13 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary13 = {...global.runJobVocabulary13, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary12 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary12;
-  } 
-
-  setRunJobVocabulary12 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary12 = {...global.runJobVocabulary12, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary11 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary11;
-  } 
-
-  setRunJobVocabulary11 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary11 = {...global.runJobVocabulary11, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary10 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary10;
-  } 
-
-  setRunJobVocabulary10 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary10 = {...global.runJobVocabulary10, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary9 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary9;
-  } 
-
-  setRunJobVocabulary9 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary9 = {...global.runJobVocabulary9, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary8 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary8;
-  } 
-
-  setRunJobVocabulary8 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary8 = {...global.runJobVocabulary8, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary7 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary7;
-  } 
-
-  setRunJobVocabulary7 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary7 = {...global.runJobVocabulary7, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary6 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary6;
-  } 
-
-  setRunJobVocabulary6 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary6 = {...global.runJobVocabulary6, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary5 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary5;
-  } 
-
-  setRunJobVocabulary5 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary5 = {...global.runJobVocabulary5, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary4 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary4;
-  } 
-
-  setRunJobVocabulary4 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary4 = {...global.runJobVocabulary4, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary3 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary3;
-  } 
-
-  setRunJobVocabulary3 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary3 = {...global.runJobVocabulary3, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary2 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary2;
-  } 
-
-  setRunJobVocabulary2 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary2 = {...global.runJobVocabulary2, updatedAt: moment().valueOf(), ...value };
-  }
-
-  getRunJobVocabulary1 = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
-    return global.runJobVocabulary1;
-  } 
-
-  setRunJobVocabulary1 = (value: { status?: Boolean; browser?: Browser; }) => {
-    global.runJobVocabulary1 = {...global.runJobVocabulary1, updatedAt: moment().valueOf(), ...value };
   }
 
   getRunJobVocabulary = (): { status: Boolean; browser: Browser; updatedAt: number; } => {
